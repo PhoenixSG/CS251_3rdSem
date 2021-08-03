15cca00ab058de1c8eaf58ac1702c85150778523
A Basu <81401491+arponbasu@users.noreply.github.com>
2021-08-03 13:52:05 +0530

    Shashwat wrote evaluate.sh

--- Outlab_1/200050013-200050130/Code/download.sh
+++ Outlab_1/200050013-200050130/Code/download.sh
@@ -5,5 +5,11 @@ if [[ $# -ne 2 ]]; then
     exit 1
 fi
 
-wget -rqx -nH -np --reject "index.html{}, index.html, index.html.tmp" --cut-dirs=$2 $1
+filename=$(basename $1)
 
+wget  -rqx -nH -np --reject "index.html{}, index.html, index.html.tmp" --cut-dirs=$2 $1
+
+if [ $filename != "mock_grading" ]
+then
+	mv $filename mock_grading
+fi
--- Outlab_1/200050013-200050130/Code/evaluate.sh
+++ Outlab_1/200050013-200050130/Code/evaluate.sh
@@ -1,2 +1,40 @@
 #!/bin/bash
 
+touch marksheet.csv
+touch distribution.txt
+
+
+cd organised/
+
+find . -maxdepth 1 -mindepth 1 -type d -printf '%f\n' | while read line || [[ -n $line ]]; do
+
+   for j in $line/*; do
+    #echo $j 
+    mkdir -p $line/student_outputs
+    marks=0
+    if [[ $j == $line*.cpp ]]; then
+    	#echo $j "check"
+		g++ -o $line/executable $j  2>/dev/null		
+		#echo "first"
+		for iter in ../mock_grading/inputs/*;do
+			filename=$(basename $iter .in)
+			#echo $iter
+			#echo $filename
+			./$line/executable  < $iter > $line/student_outputs/$filename.out 2>/dev/null & sleep 1
+			cmp -s $line/student_outputs/$filename.out ../mock_grading/outputs/$filename.out
+			ans=$?
+			#echo $ans
+			if [[ $ans==0 ]]
+			then
+				marks=$(($marks+1))
+				#echo $marks
+			fi
+			#echo "here"
+		done
+		printf "$line,$marks\n" >> ../marksheet.csv
+    fi   
+done    
+done
+
+
+
