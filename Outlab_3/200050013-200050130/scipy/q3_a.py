ac3641d5f03d4a29bf5fd33aa67b3369ca02ac2a
A Basu <81401491+arponbasu@users.noreply.github.com>
2021-08-20 02:35:35 +0530

    sys replaced by argparse. Test thoroughly

--- Outlab_3/200050013-200050130/scipy/q3_a.py
+++ Outlab_3/200050013-200050130/scipy/q3_a.py
@@ -1,9 +1,19 @@
 from scipy.integrate import tplquad
-import sys
+import argparse
+
+
+p = argparse.ArgumentParser()
+p.add_argument('values', nargs = '*', type = float)
+args = p.parse_args()
+
+numList = args.values
+
+
+
 
 func = lambda x,y,z: (x**2) * (y**2) * (z**2)
-x1,x2 = float(sys.argv[1]), float(sys.argv[2])
-y1,y2 = lambda x: float(sys.argv[3]), lambda x: float(sys.argv[4])
-z1,z2 = lambda x,y: float(sys.argv[5]), lambda x,y: float(sys.argv[6])
-print(tplquad( func, x1, x2, y1, y2, z1, z2 )[0])
+x1,x2 = numList[0], numList[1]
+y1,y2 = lambda x: numList[2], lambda x: numList[3]
+z1,z2 = lambda x,y: numList[4], lambda x,y: numList[5]
+print(tplquad(func, x1, x2, y1, y2, z1, z2)[0])
 
