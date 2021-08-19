46039cea3ff20348dff1fa263b7c6dc893710577
A Basu <81401491+arponbasu@users.noreply.github.com>
2021-08-20 02:38:29 +0530

    sys replaced by argparse. Test thoroughly on docker

--- Outlab_3/200050013-200050130/numpy/q2_b/pca.py
+++ Outlab_3/200050013-200050130/numpy/q2_b/pca.py
@@ -2,18 +2,21 @@ import pandas as pd
 import numpy as np
 import matplotlib
 from numpy import linalg as LA
-import sys
 import matplotlib.pyplot as plt
 matplotlib.use('Agg')
 
-a = pd.read_csv(str(sys.argv[2]), header = None)
+import argparse
+ap = argparse.ArgumentParser()
+ap.add_argument("--path", required = True)
+ap.add_argument("--output", required = True)
+args = vars(ap.parse_args())
+
+print(str(args['path']))
+
+a = pd.read_csv(str(args['path']), header = None)
 a -= a.mean()
-#print(a)
-#print(np.cov(a.transpose()))
 eigval, eigvec = LA.eig(np.cov(a.transpose()))
-#print(eigval)
 eigvec = eigvec.transpose()
-#print(eigvec)
 eigval, eigvec = zip(*sorted(zip(eigval, eigvec), reverse = True))
 
 eigval = [c for c in eigval] 
@@ -25,16 +28,11 @@ eigvec_2 = [c for c in eigvec[1]]
 
 
 print(eigval)
-#print(eigvec)
-
-#print(np.concatenate((eigvec_1, eigvec_2)).reshape((-1, 2), order = 'F'))
 transformed = a.dot(np.concatenate((eigvec_1, eigvec_2)).reshape((-1, 2), order = 'F'))
 transformed = np.array(transformed)
-#print(transformed.shape)
-#print(transformed)
 
 
-st = str(sys.argv[4])
+st = str(args['output'])
 
 np.savetxt(st + "transform.csv", transformed, delimiter = ",")
 
@@ -43,7 +41,5 @@ sz = 15.0
 plt.xlim(-sz, sz)
 plt.ylim(-sz, sz)
 plt.gca().set_aspect('equal', adjustable = 'box')
-#plt.show()
 f = open(st + 'out.png', 'w')
 plt.savefig(st + 'out.png')
-
