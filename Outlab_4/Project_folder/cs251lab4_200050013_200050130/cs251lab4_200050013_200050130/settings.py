9d358c6629508eb9e9d5f2a2518df0d5ce2e180f
Shashwat <shashwatg2002@gmail.com>
2021-08-25 15:34:25 +0530

    further developments

--- Outlab_4/Project_folder/cs251lab4_200050013_200050130/cs251lab4_200050013_200050130/settings.py
+++ Outlab_4/Project_folder/cs251lab4_200050013_200050130/cs251lab4_200050013_200050130/settings.py
@@ -15,12 +15,15 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-askyxsus$(8*w=b&ubgbaerkuw)bxjqz4lmroe^k7ajc5br5*!'
+#SECRET_KEY = 'django-insecure-askyxsus$(8*w=b&ubgbaerkuw)bxjqz4lmroe^k7ajc5br5*!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
