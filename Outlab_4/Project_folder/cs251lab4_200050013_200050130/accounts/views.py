e6f28e3e4d648d81a3efb42cbc39aa590293dc57
A Basu <81401491+arponbasu@users.noreply.github.com>
2021-09-06 21:40:57 +0530

    Changed views.py to add two extra fields

--- Outlab_4/Project_folder/cs251lab4_200050013_200050130/accounts/views.py
+++ Outlab_4/Project_folder/cs251lab4_200050013_200050130/accounts/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
-
-# Create your views here.
-# accounts/views.py
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from accounts.forms import SignUpForm
+from django.contrib.auth.forms import UserCreationForm, authenticate
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views import generic
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+def signup_view(request):
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        form.save()
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
