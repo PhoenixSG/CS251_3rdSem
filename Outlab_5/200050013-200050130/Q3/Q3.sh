a22101199eab1e917bcbeef12b6b113ab323c095
A Basu <81401491+arponbasu@users.noreply.github.com>
2021-09-21 21:47:00 +0530

    Fix ham and spam.txt

---
+++ Outlab_5/200050013-200050130/Q3/Q3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+rm -f ham.txt
+touch ham.txt
+awk '/0:/' sample.txt | while read line || [[ -n $line ]]; do
+    tmp=${line#*:} 
+    arr=($(echo "$tmp" | tr "|" "\n"))
+    for i in "${arr[@]}"; do
+        printf "$i\n" >> ham.txt
+    done
+done
+awk -F '-' 'FNR==NR{a[$2]=$1;next} a[$1]{print a[$1] >"ham.txt"}' word_token_mapping.txt ham.txt 
+echo $(sort ham.txt | uniq -c | sort -k1,1nr -k2|sed 's/^\s*[0-9]* \(.*\)$/\1/') > ham.txt
+
+rm -f spam.txt
+touch spam.txt
+awk '/1:/' sample.txt | while read line || [[ -n $line ]]; do
+    tmp=${line#*:} 
+    arr=($(echo "$tmp" | tr "|" "\n"))
+    for i in "${arr[@]}"; do
+        printf "$i\n" >> spam.txt
+    done
+done
+awk -F '-' 'FNR==NR{a[$2]=$1;next} a[$1]{print a[$1] >"spam.txt"}' word_token_mapping.txt spam.txt 
+echo $(sort spam.txt | uniq -c | sort -k1,1nr -k2|sed 's/^\s*[0-9]* \(.*\)$/\1/') > spam.txt
+
