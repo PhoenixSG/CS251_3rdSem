8a48afd3b88cad4ebbede40c6df46bb39fa5241f
A Basu <81401491+arponbasu@users.noreply.github.com>
2021-09-23 02:34:58 +0530

    All loops translated to awk. Should be acceptable now ig

--- Outlab_5/200050013-200050130/Q3/Q3.sh
+++ Outlab_5/200050013-200050130/Q3/Q3.sh
@@ -1,42 +1,63 @@
 #!/bin/bash
-cnt=$1
+v=$1
 rm -f ham.txt
 touch ham.txt
-awk '/0:/' sample.txt | while read line || [[ -n $line ]]; do
-    tmp=${line#*:} 
-    arr=($(echo "$tmp" | tr "|" "\n"))
-    for i in "${arr[@]}"; do
-        printf "$i\n" >> ham.txt
-    done
-done
-awk -F '-' 'FNR==NR{a[$2]=$1;next} a[$1]{print a[$1] >"ham.txt"}' word_token_mapping.txt ham.txt 
-echo $(sort ham.txt | uniq -c | sort -k1,1nr -k2| sed 's/^\s*[0-9]* \(.*\)$/\1/') > ham.txt
 touch tmp.txt
-cat ham.txt | while read line || [[ -n $line ]]; do
-    for i in $line; do
-        printf "$i\n" >> tmp.txt
-    done
-done
-head -n$cnt tmp.txt > ham.txt
+awk -F "|" '/0:/ {print substr($0,3)}' sample.txt | awk '
+BEGIN{
+FS = "|";
+}
+{
+    for(i = 1; i <= NF; i++){
+        if($i in arr) arr[$i]++;
+        else arr[$i] = 1;
+    }
+}
+END{
+for (i in arr) for(j = 0; j < arr[i]; ++j) print i
+}
+' > ham.txt 
+awk -F '-' 'FNR==NR{a[$2]=$1;next} a[$1]{print a[$1] >"ham.txt"}' word_token_mapping.txt ham.txt 
+echo $(sort ham.txt | uniq -c | sort -k1,1nr -k2| sed 's/^\s*[0-9]* \(.*\)$/\1/') > ham.txt #Sorting deliberately done through bash and not awk due to portability #issues
+awk -v v="$v" '
+BEGIN{
+FS = " ";
+}
+{
+    if(NF > v) for(i = 1; i <= v; i++) print $i;
+    else for(i = 1; i <= NF; i++) print $i;
+}
+' ham.txt > tmp.txt
+mv tmp.txt ham.txt
 rm -f tmp.txt
 
 rm -f spam.txt
 touch spam.txt
-awk '/1:/' sample.txt | while read line || [[ -n $line ]]; do
-    tmp=${line#*:} 
-    arr=($(echo "$tmp" | tr "|" "\n"))
-    for i in "${arr[@]}"; do
-        printf "$i\n" >> spam.txt
-    done
-done
-awk -F '-' 'FNR==NR{a[$2]=$1;next} a[$1]{print a[$1] >"spam.txt"}' word_token_mapping.txt spam.txt 
-echo $(sort spam.txt | uniq -c | sort -k1,1nr -k2|sed 's/^\s*[0-9]* \(.*\)$/\1/') > spam.txt
 touch tmp.txt
-cat spam.txt | while read line || [[ -n $line ]]; do
-    for i in $line; do
-        printf "$i\n" >> tmp.txt
-    done
-done
-head -n$cnt tmp.txt > spam.txt
+awk -F "|" '/1:/ {print substr($0,3)}' sample.txt | awk '
+BEGIN{
+FS = "|";
+}
+{
+    for(i = 1; i <= NF; i++){
+        if($i in arr) arr[$i]++;
+        else arr[$i] = 1;
+    }
+}
+END{
+for (i in arr) for(j = 0; j < arr[i]; ++j) print i
+}
+' > spam.txt 
+awk -F '-' 'FNR==NR{a[$2]=$1;next} a[$1]{print a[$1] >"spam.txt"}' word_token_mapping.txt spam.txt 
+echo $(sort spam.txt | uniq -c | sort -k1,1nr -k2| sed 's/^\s*[0-9]* \(.*\)$/\1/') > spam.txt #Sorting deliberately done through bash and not awk due to portability #issues
+awk -v v="$v" '
+BEGIN{
+FS = " ";
+}
+{
+    if(NF > v) for(i = 1; i <= v; i++) print $i;
+    else for(i = 1; i <= NF; i++) print $i;
+}
+' spam.txt > tmp.txt
+mv tmp.txt spam.txt
 rm -f tmp.txt
-
