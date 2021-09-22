9ed4a0ec04d3e0b190ff50a0ef782337bd329d8b
Shashwat <shashwatg2002@gmail.com>
2021-09-22 11:41:41 +0530

    Q5 partly done, output not matching perfectly...

---
+++ Outlab_5/200050013-200050130/Q5/Q5.sh
@@ -0,0 +1,50 @@
+#! /usr/bin/awk -f 
+BEGIN{
+    FS=">"
+}
+/<div class="field field-name-field-select-state field-type-list-text field-label-above"><div class="field-label">State Name/{
+    # print($6,"  ", $14)
+    split($6, name, "<")
+    split($14, number, "<")
+    # print(name[1], number[1])
+    # for(i=0; i<NF; i=i+1){
+    #     print($i)
+    # }
+    # if(name[1])
+    arr[name[1]] = number[1]    
+}
+{
+    # print(arr[$0])
+    if(FNR!=NR){
+        split($1, array, "\r")
+        for (key in arr){
+        # if()
+            if(key==array[1]){
+                print(key, arr[key]) | "sort"
+                # print($0) |"sort"
+                # print($0, arr[$0]) | "sort"
+            }
+        }
+    }
+}
+END{
+    
+    # print(list)
+
+    # for (key in arr){
+    #     # if()
+    #     print(key, arr[key])
+    # }
+    # while((getline state < "sample.txt")>0){
+    #     word = state
+    #     print(word)
+    #     # if(state in arr){
+    #         print(word, arr[word]) | "sort"
+    #     # }
+    # }
+}
+
+#toggel arr and  time array
+# convert time to numbers and add and subtract
+# easy logic notmuch
+# sort while printing at the end
