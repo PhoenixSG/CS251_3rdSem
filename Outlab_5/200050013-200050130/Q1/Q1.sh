4bd2e757126822b1a18942b1da7281614e074dd6
A Basu <81401491+arponbasu@users.noreply.github.com>
2021-09-22 19:20:57 +0530

    Q1 done mostly. Just rename Work.txt to sample.txt at last moment

---
+++ Outlab_5/200050013-200050130/Q1/Q1.sh
@@ -0,0 +1,15 @@
+sed -e 's!www.[^[:space:]]*!!g' -e 's!http\(s\)\{0,1\}://[^[:space:]]*!!g' -e 's/.*/\L&/g' -e 's/^ *\| *$//g' -e $'s/\t//g' -e "s/[[:punct:]]\+//g" -re 's/[^[:space:]]*[0-9][^[:space:]]* ?//g' -re 's/\b\w{1,2}\s?\b//g' -e '/^$/d' <Work.txt >MyOutput.txt
+mapfile -t array < stopwords.txt 
+for i in "${array[@]}"
+do
+    sed -ie "s/\<$i\>\s*//g" MyOutput.txt #Removes stopwords
+done 
+mapfile -t array2 < suffixes.txt 
+for i in "${array2[@]}"
+do 
+    sed -ier "s/${i}\b//g" MyOutput.txt #Removes suffixes
+done 
+sed -ie '/^[[:space:]]*$/d' MyOutput.txt
+rm -f MyOutput.txte
+rm -f MyOutput.txter
+rm -f MyOutput.txterE
