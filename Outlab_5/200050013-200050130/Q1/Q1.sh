23f5f9ad619f54c6079b8cd5fe96c4c4e2c1a395
A Basu <81401491+arponbasu@users.noreply.github.com>
2021-09-24 20:24:37 +0530

    Removed 2 redundant tags

--- Outlab_5/200050013-200050130/Q1/Q1.sh
+++ Outlab_5/200050013-200050130/Q1/Q1.sh
@@ -1,4 +1,4 @@
-sed -e 's!www.[^[:space:]]*!!g' -e 's!http\(s\)\{0,1\}://[^[:space:]]*!!g' -e 's/.*/\L&/g' -e 's/^ *\| *$//g' -e $'s/\t//g' -e "s/[[:punct:]]\+//g" -re 's/[^[:space:]]*[0-9][^[:space:]]* ?//g' -re 's/\b\w{1,2}\s?\b//g' -e '/^$/d' <sample.txt >MyOutput.txt
+sed -e 's!www.[^[:space:]]*!!g' -e 's!http\(s\)\{0,1\}://[^[:space:]]*!!g' -e 's/.*/\L&/g' -e 's/^ *\| *$//g'  -e "s/[[:punct:]]\+//g" -re 's/[^[:space:]]*[0-9][^[:space:]]* ?//g' -re 's/\b\w{1,2}\s?\b//g' <sample.txt >MyOutput.txt
 mapfile -t array < stopwords.txt 
 for i in "${array[@]}"
 do
