85dc58889d870d28f0a306fb58aa09278b44fec1
A Basu <81401491+arponbasu@users.noreply.github.com>
2021-09-24 23:46:31 +0530

    Matched sample_output.txt perfectly

--- Outlab_5/200050013-200050130/Q1/Q1.sh
+++ Outlab_5/200050013-200050130/Q1/Q1.sh
@@ -1,13 +1,16 @@
-sed -e 's!www.[^[:space:]]*!!g' -e 's!http\(s\)\{0,1\}://[^[:space:]]*!!g' -e 's/.*/\L&/g' -e 's/^ *\| *$//g'  -e "s/[[:punct:]]\+//g" -re 's/[^[:space:]]*[0-9][^[:space:]]* ?//g' -re 's/\b\w{1,2}\s?\b//g' <sample.txt >MyOutput.txt
+sed -e 's!www.[^[:space:]]*!!g' -e 's!http\(s\)\{0,1\}://[^[:space:]]*!!g' -e 's/.*/\L&/g' -e 's/^ *\| *$//g'  -e "s/[[:punct:]]\+//g" -re 's/[^[:space:]]*[0-9][^[:space:]]* ?//g' -re 's/\b\w{1,2}\s?\b//g' <sample.txt >output.txt
 mapfile -t array < stopwords.txt 
 for i in "${array[@]}"
 do
-    sed -i "s/\<$i\>\s*//g" MyOutput.txt #Removes stopwords
+    sed -i "s/\<$i\>\s*//g" output.txt #Removes stopwords
 done 
 mapfile -t array2 < suffixes.txt 
 for i in "${array2[@]}"
 do 
-    sed -i "s/${i}\b//g" MyOutput.txt #Removes suffixes
+    sed -i "s/${i}\b/A/g" output.txt #Replaces suffixes with dummy character 'A', guaranteed not to occur anywhere else since all letters have been lowercased
 done 
-sed -ie '/^[[:space:]]*$/d' MyOutput.txt
-rm -f MyOutput.txte
+sed -i -e "s/A\+//g" output.txt #Removes the 'A's introduced above (The 'A's basically ensure that each word is stemmed atmost once)
+sed -i -e 's/[[:blank:]]*$//' output.txt #Trims spaces at end of lines
+sed -i -e '/^[[:space:]]*$/d' output.txt #Trims spaces at beginnings of lines
+sed -i -e "s/[[:space:]]\{2,\}/ /g" output.txt #Makes spacing uniform
+
