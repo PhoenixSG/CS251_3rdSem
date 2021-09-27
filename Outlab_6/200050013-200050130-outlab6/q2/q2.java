8b9b3c9b97e5e9185722ba08f2e9d4cf537f9731
A Basu <81401491+arponbasu@users.noreply.github.com>
2021-09-27 15:53:37 +0530

    Q2 done. Should I add '\n' at end of file?

--- Outlab_6/200050013-200050130-outlab6/q2/q2.java
+++ Outlab_6/200050013-200050130-outlab6/q2/q2.java
@@ -1,7 +1,56 @@
-public class q2
-{
-    public static void main(String[] args)
-    {
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class q2{
     
+    public static void main(String[] args) throws IOException{
+        HashMap<Character, Integer> freqTable = new HashMap<Character, Integer>();
+        BufferedReader r = new BufferedReader(new FileReader("input.txt"));
+        int ch;
+        while((ch = r.read()) != -1){             
+            if(('a' <= ch) && (ch <= 'z')) {
+                char elem = (char) ch;
+                if(freqTable.get(elem) != null){
+                    int value = freqTable.get(elem);
+                    value++;
+                    freqTable.put(elem,value);
+                }
+                else freqTable.put(elem,1);
+            }
+            else if(('A' <= ch) && (ch <= 'Z')){
+                ch += ('a' - 'A');
+                char elem = (char) ch;
+                if(freqTable.get(elem) != null){
+                    int value = freqTable.get(elem);
+                    value++;
+                    freqTable.put(elem,value);
+                }
+                else freqTable.put(elem,1);
+
+            }          
+        }
+        LinkedHashMap<Character, Integer> result = 
+        freqTable.entrySet().stream()
+                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed()
+                             .thenComparing(Map.Entry.comparingByKey()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+        int init = -1, it = 0;
+        String answer = "";
+        for (Character name: result.keySet()) {
+            ++it;
+            String key = name.toString();
+            Integer val = result.get(name);
+            String value = val.toString();
+            if(val == init) answer += ("," + key + "=" + value);
+            else if(it == 1) answer += (key + "=" + value);
+            else answer += ("\n" + key + "=" + value);
+            init = val;
+        }
+        Writer br = new FileWriter(new File("output.txt"));
+        br.write(answer);
+        br.flush(); 
+        br.close();
     }   
 }
