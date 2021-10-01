e50388b7a88c5269c2b70a4011f2d9326215ea91
A Basu <81401491+arponbasu@users.noreply.github.com>
2021-10-02 01:00:35 +0530

    Q2 rectified

--- Outlab_6/200050013-200050130-outlab6/q2/q2.java
+++ Outlab_6/200050013-200050130-outlab6/q2/q2.java
@@ -3,54 +3,52 @@ import java.util.*;
 import java.util.stream.*;
 
 public class q2{
-    
     public static void main(String[] args) throws IOException{
-        HashMap<Character, Integer> freqTable = new HashMap<Character, Integer>();
-        BufferedReader r = new BufferedReader(new FileReader("input.txt"));
-        int ch;
-        while((ch = r.read()) != -1){             
-            if(('a' <= ch) && (ch <= 'z')) {
-                char elem = (char) ch;
-                if(freqTable.get(elem) != null){
-                    int value = freqTable.get(elem);
-                    value++;
-                    freqTable.put(elem,value);
-                }
-                else freqTable.put(elem,1);
-            }
-            else if(('A' <= ch) && (ch <= 'Z')){
-                ch += ('a' - 'A');
-                char elem = (char) ch;
-                if(freqTable.get(elem) != null){
-                    int value = freqTable.get(elem);
-                    value++;
-                    freqTable.put(elem,value);
-                }
-                else freqTable.put(elem,1);
+            BufferedReader br = new BufferedReader(new FileReader("input.txt")) ;
+			String str, answer = "";
+			while ((str = br.readLine()) != null) {
+                  HashMap<Character, Integer> freqTable = new HashMap<Character, Integer>();
+                  for(int i = 0; i < str.length(); ++i){
+                        char ch = str.charAt(i);
+                        if(('a' <= ch) && (ch <= 'z')) {
+                            if(freqTable.get(ch) != null){
+                                int value = freqTable.get(ch);
+                                value++;
+                                freqTable.put(ch,value);
+                            }
+                            else freqTable.put(ch,1);
+                        }
+                        else if(('A' <= ch) && (ch <= 'Z')){
+                            ch += ('a' - 'A');
+                            if(freqTable.get(ch) != null){
+                                int value = freqTable.get(ch);
+                                value++;
+                                freqTable.put(ch,value);
+                            }
+                            else freqTable.put(ch,1);
 
-            }          
-        }
-        LinkedHashMap<Character, Integer> result = 
-        freqTable.entrySet().stream()
-                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed()
-                             .thenComparing(Map.Entry.comparingByKey()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
-        int init = -1, it = 0;
-        String answer = "";
-        for (Character name: result.keySet()) {
-            ++it;
-            String key = name.toString();
-            Integer val = result.get(name);
-            String value = val.toString();
-            if(val == init) answer += ("," + key + "=" + value);
-            else if(it == 1) answer += (key + "=" + value);
-            else answer += ("\n" + key + "=" + value);
-            init = val;
-        }
-        Writer br = new FileWriter(new File("output.txt"));
-        br.write(answer);
-        br.flush(); 
-        br.close();
-    }   
+                        } 
+                    }         
+                    LinkedHashMap<Character, Integer> result = freqTable.entrySet().stream().sorted(Map.Entry.<Character, Integer>comparingByValue().reversed()
+                    .thenComparing(Map.Entry.comparingByKey())).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                    (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+                    int init = result.entrySet().iterator().next().getValue(), iter = 0;
+                    for (Character name: result.keySet()) {
+                        ++iter;
+                        if(result.get(name) == init) {
+                            if(iter == 1) answer += (name + "=" + result.get(name));
+                            else answer += ("," + name + "=" + result.get(name));
+                        }
+                        else break;
+                    }
+                    answer += ("\n");
+			}
+            //System.out.print(answer);
+            Writer wr = new FileWriter(new File("output.txt"));
+            wr.write(answer);
+            wr.flush();
+            wr.close();
+    }
 }
+
+
