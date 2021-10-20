19500dd526859b64c3035bee32c0f000c866f253
Shashwat Garg <shashwatg2002@gmail.com>
2021-10-20 11:48:39 +0530

    Merged changes

--- Outlab_7/200050013-200050130/bobby/ServerThread.java
+++ Outlab_7/200050013-200050130/bobby/ServerThread.java
@@ -416,8 +416,7 @@ public class ServerThread implements Runnable{
 
 				Reuse count to keep track of how many threads hit this barrier2 
 
-				The code is similar. However, the last thread to hit this barrier must also 
-				permit the moderator to run
+				The code is similar.
 				*/
 				                                    
                        
@@ -434,7 +433,12 @@ public class ServerThread implements Runnable{
 				actually finishing off a thread
 				that decided to quit
 
-				If you quit while reading, now is the time to erase the player
+				However, the last thread to hit this must issue one
+				permit for the moderator to run
+
+				If all else fails use the barriers again
+
+				//If you quit while reading, now is the time to erase the player
 				*/
 				          
                              
