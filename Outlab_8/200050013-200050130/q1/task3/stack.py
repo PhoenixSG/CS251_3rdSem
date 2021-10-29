188a85e2d71870845776caf6515f59a73233aba8
Shashwat Garg <shashwatg2002@gmail.com>
2021-10-29 17:29:56 +0530

    Trying task 3 question1

--- Outlab_8/200050013-200050130/q1/task3/stack.py
+++ Outlab_8/200050013-200050130/q1/task3/stack.py
@@ -1,8 +1,12 @@
 import gdb
 
 def printer(event):
-    #rsp = int(gdb.parse_and_eval("$rsp"))
-    #rbp = int(gdb.parse_and_eval("$rbp"))
+    rsp = int(gdb.parse_and_eval("$rsp"))
+    rbp = int(gdb.parse_and_eval("$rbp"))
+    print("+-------------------------+")
+    print(rsp)
+
+    print(rbp)
     
 
 gdb.events.stop.connect(printer)
