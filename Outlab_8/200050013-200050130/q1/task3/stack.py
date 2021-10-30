7e625a65e56196f884c4c3e396ef5cf088827ef0
Shashwat Garg <shashwatg2002@gmail.com>
2021-10-30 22:38:35 +0530

    Some more work on task 3, not sure if correct but output format matches

--- Outlab_8/200050013-200050130/q1/task3/stack.py
+++ Outlab_8/200050013-200050130/q1/task3/stack.py
@@ -1,12 +1,31 @@
 import gdb
 
 def printer(event):
-    rsp = int(gdb.parse_and_eval("$rsp"))
-    rbp = int(gdb.parse_and_eval("$rbp"))
-    print("+-------------------------+")
-    print(rsp)
+    i=0
+    answer=""
+    while(True):
+        a = gdb.execute("x/8bx $rsp+"+str(8*i), from_tty=True, to_string=True)
+        addresses = a.split('0x')
+        addresses = addresses[2:]
+        answer+="+-------------------------+\n"
+        answer+="| "
+        for j in range(len(addresses)):
+            answer+= addresses[j][:2]+" "
+        rsp = int(gdb.parse_and_eval("$rsp+"+str(8*i)))
+        rbp = int(gdb.parse_and_eval("$rbp"))
+        if i==0:
+            answer+="| <- rsp\n"
+        elif rsp>=rbp:
+            answer+="| <- rbp\n"
+            break
+        else:
+            answer+="| \n"
+            
+        i+=1
+    
+    answer+="+-------------------------+"
 
-    print(rbp)
+    print(answer)
     
 
 gdb.events.stop.connect(printer)
