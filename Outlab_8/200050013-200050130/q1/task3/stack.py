92257df6621e8efee5112d304a5b7467855db900
Shashwat <shashwatg2002@gmail.com>
2021-11-06 14:50:49 +0530

    Task 3 done!

--- Outlab_8/200050013-200050130/q1/task3/stack.py
+++ Outlab_8/200050013-200050130/q1/task3/stack.py
@@ -12,69 +12,52 @@ When a.older() gives a None object, a should be the last frame.
 Now within the last frame, you can do
 val_of_rbp = gdb.execute(<command>, from_tty= True, to_string=True)
 '''
+base_rbp = -1
+
 def printer(event):
-    i=0
-    answer=""
-    initial_rsp = int(gdb.parse_and_eval("$rsp"))
+
+    rsp = int(gdb.parse_and_eval("$rsp"))
     rbp = int(gdb.parse_and_eval("$rbp"))
-    while(True):
-        a = str(gdb.execute("x/8bx $rsp+"+str(8*i), to_string=True, from_tty=True))
-        addresses = a.split('0x')
-        addresses = addresses[2:]
-        answer+="+-------------------------+\n"
-        answer+="| "
-        for j in range(len(addresses)):
-            answer+= addresses[j][:2]+" "
-        rsp = int(gdb.parse_and_eval("$rsp+"+str(8*i)))
-        print(rsp)
-        if i==0:
-            answer+="| <- rsp"
-            if rsp>=rbp:
-                answer+=" rbp\n"
+    # print(rsp)
+    # print(rbp)
+    answer=""
+    global base_rbp
+    if base_rbp==-1:
+        if rbp>=rsp:
+            base_rbp = rbp
+    else:
+        i=0
+        while(True):
+            a = str(gdb.execute("x/8bx $rsp+"+str(8*i), to_string=True))
+            addresses = a.split('0x')
+            addresses = addresses[2:]
+            answer+="+-------------------------+\n"
+            answer+="| "
+            for j in range(len(addresses)):
+                answer+= addresses[j][:2]+" "
+            rsp = int(gdb.parse_and_eval("$rsp+"+str(8*i)))
+            # print(rsp)
+            # print(rbp)
+            # print(base_rbp)
+            if i==0:
+                answer+="| <- rsp"
+                if rsp+8>=rbp:
+                    answer+=" rbp\n"
+                    if rsp>=base_rbp:
+                        break
+                else:
+                    answer+="\n"
+            elif rsp>=rbp and rsp-8<rbp:
+                answer+="| <- rbp\n"
+                if rsp>=base_rbp:
+                    break
             else:
-                answer+="\n"
-        elif rsp>=rbp:
-            answer+="| <- rbp\n"
+                answer+="|\n"
+                if rsp>=base_rbp:
+                    break
             i+=1
-            break
-        else:
-            answer+="|\n"
-            
-        i+=1
     
-
-
-    frame = gdb.newest_frame().older()
-    while frame!=None :
-        frame.select()
-        print('x')
-        frame = frame.older()
-
-    print('a')
-    new_rbp = int(gdb.parse_and_eval("$rbp"))
-
-    print(i)
-    while(True):
-        a = str(gdb.execute("x/8bx $rsp+"+str(8*i), to_string=True, from_tty=True))
-        rsp = int(gdb.parse_and_eval("$rsp+"+str(8*i)))
-        if rsp>=new_rbp:
-            break
-        addresses = a.split('0x')
-        addresses = addresses[2:]
-        answer+="+-------------------------+\n"
-        answer+="| "
-        for j in range(len(addresses)):
-            answer+= addresses[j][:2]+" "
-        print(rsp)
-        answer+="|\n"
-        i+=1
-
-
-    print('b')
-    print(initial_rsp)
-    print(rbp)
-    print(new_rbp)
-    answer+="+-------------------------+"
+        answer+="+-------------------------+"
     print(answer)
 
 gdb.events.stop.connect(printer)
--- Outlab_8/200050013-200050130/q1/task3/x.gdb
+++ Outlab_8/200050013-200050130/q1/task3/x.gdb
@@ -1,7 +1,7 @@
 source stack.py
-b 9
 b 16
 r
-c
-c
+step
+step
+step
 q
