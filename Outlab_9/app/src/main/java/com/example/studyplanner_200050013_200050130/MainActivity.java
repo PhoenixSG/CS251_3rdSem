1d7ae577c44d0ee8b57bf97a9132cbee3f4f39a6
Shashwat Garg <shashwatg2002@gmail.com>
2021-11-08 17:30:42 +0530

    Adding outlab 9

---
+++ Outlab_9/app/src/main/java/com/example/studyplanner_200050013_200050130/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.studyplanner_200050013_200050130;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
