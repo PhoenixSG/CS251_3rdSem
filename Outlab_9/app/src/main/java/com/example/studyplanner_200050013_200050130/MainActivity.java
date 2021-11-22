aa3f2221b5ecc0a6cc69e93118dd465c04d721df
arponbasu <200050013@iitb.ac.in>
2021-11-22 10:25:47 +0530

    Side Navigation Bar Added

--- Outlab_9/app/src/main/java/com/example/studyplanner_200050013_200050130/MainActivity.java
+++ Outlab_9/app/src/main/java/com/example/studyplanner_200050013_200050130/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.studyplanner_200050013_200050130;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    //private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        @Override
+        public void onBackPressed(){
+                if(drawer.isDrawerOpen(GravityCompat.START)){
+                    drawer.closeDrawer(GravityCompat.START);
+                }
+                else{
+                    super.onBackPressed();
+                }
+
+
+            }
+
+         */
     }
+
+
 }
