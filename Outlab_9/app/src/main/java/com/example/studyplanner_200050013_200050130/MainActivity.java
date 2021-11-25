a1195235a8e3ddbf29039faf6e4170a7099d2af5
Shashwat Garg <shashwatg2002@gmail.com>
2021-11-25 21:17:12 +0530

    Fragments working but deprecated

--- Outlab_9/app/src/main/java/com/example/studyplanner_200050013_200050130/MainActivity.java
+++ Outlab_9/app/src/main/java/com/example/studyplanner_200050013_200050130/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.studyplanner_200050013_200050130;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        fragmentAdapter.addFragment(new Exam(), "EXAM");
+        fragmentAdapter.addFragment(new Assignment(), "ASSIGNMENT");
+        fragmentAdapter.addFragment(new StudyPlan(), "STUDY PLAN");
+        fragmentAdapter.addFragment(new Lecture(), "LECTURE");
+
+        viewPager.setAdapter(fragmentAdapter);
+
+
     }
 }
