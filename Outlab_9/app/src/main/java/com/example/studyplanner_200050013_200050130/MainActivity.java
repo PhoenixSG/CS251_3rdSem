85bcd82333d844603c8e1638c680fe2a32869e0c
arponbasu <200050013@iitb.ac.in>
2021-12-02 17:45:59 +0530

    Added nav_bar

--- Outlab_9/app/src/main/java/com/example/studyplanner_200050013_200050130/MainActivity.java
+++ Outlab_9/app/src/main/java/com/example/studyplanner_200050013_200050130/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -12,11 +13,18 @@ public class MainActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
     private ViewPager2 viewPager2;
+    //DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    //Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        //toolbar = findViewById(R.id.toolbar);
 
         tabLayout = findViewById(R.id.tabLayout);
         viewPager2 = findViewById(R.id.viewPager);
